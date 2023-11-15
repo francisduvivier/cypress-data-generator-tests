a02a08bedceb3f37f8d6e87cdcf9c7ba91ab2426
Francis Duvivier <francisduvivier@gmail.com>
2023-11-15 22:22:10 +0100

    Avoid linting idea files

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,11 @@ module.exports = {
     'no-unused-expressions': 0,
     'import/prefer-default-export': 0,
     'implicit-arrow-linebreak': 0,
+    'import/extensions': 0,
     'operator-linebreak': 0,
     'comma-style': 0,
     'comma-dangle': 0,
+    'no-restricted-syntax': 0,
   },
-  files: ['**/*.ts', '**/*.js'],
   root: true,
 };
