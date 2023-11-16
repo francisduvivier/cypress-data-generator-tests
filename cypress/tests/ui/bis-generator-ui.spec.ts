17f1b46369322c53f63217d1145d4842ff8d0d57
Francis Duvivier <francisduvivier@gmail.com>
2023-11-16 12:18:15 +0100

    Extract form manipulation functions

--- cypress/tests/ui/bis-generator-ui.spec.ts
+++ cypress/tests/ui/bis-generator-ui.spec.ts
@@ -8,6 +8,83 @@ import {
 const { baseUrl } = Cypress.config();
 const generatorId = 'bis';
 
+// TODO Move setBooleanParam to commands
+function setBooleanParam(
+  booleanParam:
+    | {
+        nokValues: readonly [number, string];
+        name: 'isBirthdateKnown';
+        okValues: readonly [boolean, boolean];
+      }
+    | {
+        nokValues: readonly [number, string];
+        name: 'isGenderKnown';
+        okValues: readonly [boolean, boolean];
+      },
+  choice: boolean
+) {
+  const paramName = booleanParam.name;
+  const choiceXpath = xPathForInputInForm(
+    paramName,
+    `radio`,
+    generatorId,
+    choice
+  );
+
+  const otherChoiceXpath = xPathForInputInForm(
+    paramName,
+    `radio`,
+    generatorId,
+    !choice
+  );
+  it(`can set the ${paramName} toggle to ${choice}`, () => {
+    cy.xpath(choiceXpath).click();
+    cy.xpath(choiceXpath)
+      .should('exist') // You can use any Cypress assertions here
+      .click(); // For example, you can click on the radio button if needed
+    cy.xpath(choiceXpath).should('be.checked');
+    cy.xpath(otherChoiceXpath).should('not.be.checked');
+  });
+}
+
+// TODO Move setAmount to commands
+function setAmount(value: 1) {
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
+  ).should('be.visible');
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
+  ).type(String(value));
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
+  ).should('have.value', String(value));
+}
+
+// TODO Move setDate to commands
+function setDate(date: '2000-11-15') {
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
+  ).should('be.visible');
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
+  ).type(date);
+  cy.xpath(
+    xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
+  ).then((item) => {
+    expect(item.val()).to.eq(date);
+  });
+}
+
+// TODO Move clickGenerate to commands
+function clickGenerate() {
+  const buttonSelector = '#\\/' + generatorId + '-generate-button';
+  cy.get(buttonSelector).should('be.visible');
+  cy.get(buttonSelector).click();
+  const textSelector = '#' + generatorId + '-text';
+  cy.get(textSelector).should('be.visible');
+  return textSelector;
+}
+
 describe('Bis Id UI Test Suite', () => {
   before(() => {
     // Visit the page by combining baseUrl and the relative URL
@@ -27,61 +104,19 @@ describe('Bis Id UI Test Suite', () => {
       paramSamples.isGenderKnown,
     ]) {
       for (const choice of booleanParam.okValues) {
-        const paramName = booleanParam.name;
-        const choiceXpath = xPathForInputInForm(
-          paramName,
-          `radio`,
-          generatorId,
-          choice
-        );
-
-        const otherChoiceXpath = xPathForInputInForm(
-          paramName,
-          `radio`,
-          generatorId,
-          !choice
-        );
-        it(`can set the ${paramName} toggle to ${choice}`, () => {
-          cy.xpath(choiceXpath).click();
-          cy.xpath(choiceXpath)
-            .should('exist') // You can use any Cypress assertions here
-            .click(); // For example, you can click on the radio button if needed
-          cy.xpath(choiceXpath).should('be.checked');
-          cy.xpath(otherChoiceXpath).should('not.be.checked');
-        });
+        setBooleanParam(booleanParam, choice);
       }
     }
     it('Should be able to fill an amount', () => {
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
-      ).should('be.visible');
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
-      ).type('5');
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.amount.name, 'number', generatorId)
-      ).should('have.value', '5');
+      const value = paramSamples.amount.okValues[0];
+      setAmount(value);
     });
     it('Should be able to select a date', () => {
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
-      ).should('be.visible');
       const date = paramSamples.date.okValues[0];
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
-      ).type(date);
-      cy.xpath(
-        xPathForInputInFormDiv(paramSamples.date.name, 'date', generatorId)
-      ).then((item) => {
-        expect(item.val()).to.eq(date);
-      });
+      setDate(date);
     });
     it('Should be able to use the generate button', () => {
-      const buttonSelector = '#\\/' + generatorId + '-generate-button';
-      cy.get(buttonSelector).should('be.visible');
-      cy.get(buttonSelector).click();
-      const textSelector = '#' + generatorId + '-text';
-      cy.get(textSelector).should('be.visible');
+      const textSelector = clickGenerate();
       cy.get(textSelector).should('not.be.empty');
     });
   });
